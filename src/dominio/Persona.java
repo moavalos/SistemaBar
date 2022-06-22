003c38af270e958e9761ef124d8b92cae916a180
mora avalos <moavalos@alumno.unlam.edu.ar>
2022-06-22 15:42:14 -0300

    demostracion uso de hashcode, equals y compareTo(implements)

--- src/dominio/Persona.java
+++ src/dominio/Persona.java
@@ -1,6 +1,8 @@
 package dominio;
 
-public class Persona {
+import java.util.Objects;
+
+public class Persona  implements Comparable<Persona>{
 	
 	private String nombre;
 
@@ -15,6 +17,30 @@ public class Persona {
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+
+	@Override
+	public int compareTo(Persona o) {
+		// TODO Auto-generated method stub
+		//return 0;
+		return this.nombre.compareTo(o.nombre);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nombre);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		return Objects.equals(nombre, other.nombre);
 	}
 
 }
