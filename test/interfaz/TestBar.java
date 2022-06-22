9dd1803370fb7447cbc6338789b1432120189c26
mora avalos <moavalos@alumno.unlam.edu.ar>
2022-06-22 15:39:39 -0300

    test error

--- test/interfaz/TestBar.java
+++ test/interfaz/TestBar.java
@@ -1,11 +1,36 @@
 package interfaz;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import dominio.Bar;
+import dominio.Persona;
 
 public class TestBar {
 
 	@Test
-	public void xd() {
+	public void queNoSeAdmitanDuplicados() {
+		Bar barcito = new Bar();
+		Persona persona1 = new Persona("Juanchis");
+		Persona persona2 = new Persona("Juanchis");
+		barcito.agregarCliente(persona1);
+		barcito.agregarCliente(persona2);
+		Integer valorEsperado = 1;
+		Integer valorObtenido = barcito.obtenerCantidadDeClientes();
+		assertEquals(valorEsperado, valorObtenido);
+		//assertEquals(1, barcito.obtenerCantidadDeClientes());
 		
 	}
+	
+	@Test 
+	public void queSeAdmitanDistintos() {
+		
+	}
+	
+	@Test
+	public void queEstenOrdenados() {
+		
+	}
+	
 }
